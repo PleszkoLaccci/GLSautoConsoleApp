e53967bf00b17814724cc0c1f78c0413302bda6b
Laci <pleszkol@kkszki.hu>
2025-10-08 11:47:19 +0200

    Feladat1 is ready

--- GlsautoConsole/AutoAdatok.cs
+++ GlsautoConsole/AutoAdatok.cs
@@ -8,5 +8,33 @@ namespace GlsautoConsole
 {
     internal class AutoAdatok
     {
+        string datum { get; set; }
+        string sofornev { get; set; }
+        int kilometer { get; set; }
+        int csomagszam { get; set; }
+        int fogyasztas { get; set; }
+
+        public AutoAdatok(string datum, string sofornev, int kilometer, int csomagszam, int fogyasztas)
+        {
+            this.datum = datum;
+            this.sofornev = sofornev;
+            this.kilometer = kilometer;
+            this.csomagszam = csomagszam;
+            this.fogyasztas = fogyasztas;
+        }
+        public AutoAdatok(string sor)
+        {
+            string[] sorSplit = sor.Split(';');
+            this.datum = sorSplit[0];
+            this.sofornev = sorSplit[1];
+            this.kilometer = int.Parse(sorSplit[2]);
+            this.csomagszam = int.Parse(sorSplit[3]);
+            this.fogyasztas = int.Parse(sorSplit[4]);
+        }
+
+        public override string ToString()
+        {
+            return $"{datum} {sofornev} {kilometer} {csomagszam} {fogyasztas}";
+        }
     }
 }
--- GlsautoConsole/Program.cs
+++ GlsautoConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace GlsautoConsole
 {
     internal class Program
     {
+        static List<AutoAdatok> adatok = new List<AutoAdatok>();
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("GLS.txt");
+
+            while (!sr.EndOfStream)
+            { 
+                adatok.Add(new AutoAdatok(sr.ReadLine()));
+            }
+            sr.Close();
+
+
         }
     }
 }
